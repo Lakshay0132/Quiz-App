733339aa645b49c6468b91fa3b727b501d20a2d1
Lakshay0132 <60537766+Lakshay0132@users.noreply.github.com>
2021-03-26 14:06:48 +0530

    Add files via upload

---
+++ medium.js
@@ -0,0 +1,48 @@
+var i = 0,j=0,c=1;
+// to get the data from the file
+
+function get_data() {
+  fetch("./medium.json")
+    .then(function (resp) {
+      return resp.json();
+    })
+    .then(function (data) {
+      console.log(data.questions[i]);
+      console.log(data.options[j]);
+      var a = data.options[i];
+      for(j = 0; j < 4; j++) {
+         
+        document.getElementById("options").innerHTML += `<p class="opt" >${a[j]}</p>`;   //to get the options from the file
+      }
+      document.getElementById("question").innerHTML = ` Q${c}. ${data.questions[i]} `;   //to get the questions from the file
+    });
+}
+//function to make the next button working
+
+function Next() {
+    c++;
+  if (i < 4) {
+    i++;
+  } else {
+    document.getElementById("medium_body").innerHTML =
+    `<h1 class="report" > Quiz Report : your Knowledge is <span>Good</span></h1>`;
+  }
+
+  console.log(i);
+  get_data();
+  document.getElementById("options").innerHTML ="";
+}
+get_data();
+
+//to set the timer while giving the test
+var m=30
+setInterval( function(){
+  m--;
+  if (m>=0){
+    document.getElementById('timer').innerHTML=`${m}`
+  }
+  if(m===0){
+    document.getElementById("medium_body").innerHTML =
+    `<h1 class="report" > Quiz Report : your Knowledge is <span>Good</span></h1>`;
+  }
+},1100)
